22accbe5b9565e69c261a271213fd3dcb71b35e9
Leelsmuth <femianjorin@rocketmail.com>
2020-05-10 15:34:40 +0100

    dummy signup to test redux setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import Signup from './components/Signup'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to Code_Clan</h1>
+      <Signup />
     </div>
   );
 }
---
+++ src/components/Signup.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { signup } from '../state/actionCreator'
+import { connect } from 'react-redux';
+
+function Signup(props) {
+  console.log(props)
+  return (
+    <div className="App">
+      <h1>Oya Signup</h1>
+      <input type="text" />
+      <button>Submit</button>
+    </div>
+  );
+}
+
+// export default Signup;
+
+const mapStateToProps = store => {
+  return {
+    loading: store.auth.loading,
+    token: store.auth.token,
+    error: store.auth.error
+  }
+}
+
+const mapDispatchToProps = {
+  register:signup
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Signup);
