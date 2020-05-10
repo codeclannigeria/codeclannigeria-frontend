e0fc0c67fd17ffae10ca1a0b8bb0d6e23d01b421
Leelsmuth <femianjorin@rocketmail.com>
2020-05-10 15:33:32 +0100

    created reducer

---
+++ src/state/reducer.js
@@ -0,0 +1,35 @@
+import * as types from './actionType';
+
+const initialSIgnupState = {
+  loading: false,
+  token: '',
+  error: false
+};
+
+export function AuthReducer(state = initialSIgnupState, action) {
+  switch (action.type) {
+    case (types.SIGNUP_START):
+      return {
+        ...state,
+        loading: true
+      }
+
+    case (types.SIGNUP_SUCCESS):
+      return {
+        ...state,
+        loading: false,
+        token: action.payload
+      }
+
+    case (types.SIGNUP_FAILURE):
+      return {
+        ...state,
+        loading: false,
+        error: true
+      }
+
+    default:
+      return state;
+  }
+
+}
