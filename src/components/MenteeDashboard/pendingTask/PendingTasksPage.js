96af0cb5f95041a2458f9017a5319f660d407699
Onasanya Tunde <Onasanyatunde67@gmail.com>
2020-07-25 21:46:03 +0100

    Fixed the pending task page

--- src/components/MenteeDashboard/pendingTask/PendingTasksPage.js
+++ src/components/MenteeDashboard/pendingTask/PendingTasksPage.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import DashboardLayout from '../../common/DashboardLayout';
 import PendingTasks from './pendingTasks';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllTasksAction } from '../../../state/tasks/tasksActionCreator';
+import CustomLoader from '../../common/Spinner/CustomLoader';
 
 function PendingTasksPage() {
+  const tasksState = useSelector(state => state.tasks);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (!tasksState.data) {
+      dispatch(getAllTasksAction());
+    }
+  }, []);
+
+  if (tasksState.error) {
+    return 'Errro';
+  }
   return (
     <>
-      <PendingTasks />
+      {!tasksState.loading ? (
+        <PendingTasks tasksData={tasksState.data} />
+      ) : (
+        <CustomLoader />
+      )}
     </>
   );
 }
