9e988662fdbf9895ac6e048d00b1e179defb6f18
Owoyomi Daniel <owoyomidaegee@gmail.com>
2020-08-19 12:30:10 +0100

    Forgot Password
    adjusted the forgot password page

--- src/components/auth/ForgotPassword.js
+++ src/components/auth/ForgotPassword.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ForgotPasswordStyled from './ForgotPasswordStyled';
 // import { authPass } from "../../state/auth/authActionCreator"
 // import { connect, useDispatch } from "react-redux"
 // import { validateEmail } from "./utils"
@@ -17,57 +18,43 @@ function ForgotPassword({
   // });
 
   return (
-    <div>
-      <div className="container-fluid">
-        <div className="row justify-content-center">
-          <div className="col-12 col-sm-6 col-md-4">
-            <form className="form-container">
-              <div className="form-group px-3">
-                <div className="form-text mb-4">
-                  <h1 className="text-center mb-2 mt-4">Forgot Password</h1>
-                  <h3 className="text-center mt-4">
-                    Enter your email and we’d send you a link to reset your
-                    password{' '}
-                  </h3>
+    <ForgotPasswordStyled>
+      <div class="container-fluid">
+        <div class="row justify-content-center">
+          <div class="container-fluid">
+            <div class="display"></div>
+          </div>
+           
+          <div class="col-12 col-sm-6 col-md-4">
+            <form class="form-container">
+                
+              <div class="form-group px-3">
+                <div class="form-text mb-4">
+                  <h1 class="text-center mb-2">Forgot Password</h1>
+                  <p class="text-center mt-4">Enter your email and we’d send you a link to reset your password </p>
+                    
                 </div>
-
-                <div className="input-group mb-4">
-                  <div className="input-group-prepend">
-                    <span className="input-group-text">
-                      <i className="fas fa-envelope"></i>
-                    </span>
+                  
+                <div class="input-group mb-4">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text"><i class="fas fa-envelope"></i></span>
                   </div>
-                  <input
-                    typeof="text"
-                    className="form-control "
-                    placeholder="Enter your Email Address"
-                    id="forget-Email"
-                    aria-label="forget-Email"
-                    aria-describedby="forget-Email"
-                  />
+                  <input type="email" class="form-control" id="forget-Email"  aria-label="forget-Email" aria-describedby="forget-Email" />
                 </div>
-                <button
-                  typeof="submit"
-                  className="btn btn-primary w-100 border-3"
-                >
-                  Submit
-                </button>
-                <a className="text-decoration-none" href="/login/">
-                  <p className="text-center mt-5">
-                    {' '}
-                    <span>
-                      <i className="fas fa-chevron-left"></i>
-                    </span>{' '}
-                    Back to Login
-                  </p>
-                </a>
-              </div>
+                <button type="submit" class="btn btn-primary w-100 border-3">Submit</button>
+                <a class="text-decoration-none" href="/login/"><p class="text-center mt-5"> <span><i class="fas fa-chevron-left"></i></span>  Back to Login</p></a>
+              </div> 
             </form>
+            
           </div>
         </div>
       </div>
-    </div>
+    </ForgotPasswordStyled>
   );
 }
 
 export default ForgotPassword;
+
+
+
+
