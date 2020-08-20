8b64d82261d75dbfc8376d3b3ad59a6c905aec16
Onasanya Tunde <Onasanyatunde67@gmail.com>
2020-08-20 19:20:01 +0100

    Made the navbar a component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 import StyledNav from './NavbarStyled';
 import { Link } from 'react-router-dom';
-import codeClanLogo from './assets/image/codeClanLogo.png';
+import CodeClanLogo from './assets/image/codeClanLogoWhite.png';
 
 function Navbar() {
+  const [openMobileNav, setopenMobileNav] = useState(false);
+  const handleOpenMobileNav = () => {
+    setopenMobileNav(!openMobileNav);
+  };
   return (
-    <StyledNav>
-      <nav className="">
-        <header>
-          <Link to="/">
-            <img src={codeClanLogo} className="img-fluid" alt="code clan" />
-          </Link>
-        </header>
-        <ul>
-          <li>
-            <i className="fas fa-search"></i>
-          </li>
-          <li>
-            <Link className="btn btn-outline-primary" to="/login/">
-              Login
-            </Link>
-          </li>
+    <StyledNav openNav={openMobileNav}>
+      <header>
+        <div className="inner-header">
+          <div>
+            <img className="logo" src={CodeClanLogo} alt="code clan" />
+          </div>
+          <div className="menu">
+            <ul>
+              <a href="teams.html">
+                <li>Team</li>
+              </a>
+              <a href="events.html">
+                <li>Events</li>
+              </a>
+              <a href="about.html">
+                <li>About</li>
+              </a>
+              <a href="contact-us.html">
+                <li>Contact Us</li>
+              </a>
+            </ul>
+          </div>
+          <div className="menu sign desktop-sign">
+            <ul>
+              <li>
+                <Link to="/login/">Login</Link>
+              </li>
+              <li className="button">
+                <Link to="/register/">Sign Up</Link>
+              </li>
+            </ul>
+          </div>
 
-          <li>
-            <Link className="btn btn-primary" to="/register/">
-              Sign up
-            </Link>
-          </li>
-        </ul>
-      </nav>
+          <div className="toggle-btn" onClick={() => handleOpenMobileNav()}>
+            {!openMobileNav ? (
+              <i className="fas fa-bars text-white"></i>
+            ) : (
+              <i className="fas fa-times  text-white"></i>
+            )}
+          </div>
+          <div className="mobile-menu">
+            <div>
+              <ul>
+                <a href="teams.html">
+                  <li>Team</li>
+                </a>
+                <a href="events.html">
+                  <li>Events</li>
+                </a>
+                <a href="about.html">
+                  <li>About</li>
+                </a>
+                <a href="contact-us.html">
+                  <li>Contact Us</li>
+                </a>
+              </ul>
+            </div>
+            <div className="menu sign mobile-sign">
+              <ul>
+                <li>
+                  <Link to="/login/">Login</Link>
+                </li>
+                <li className="mobile-button">
+                  <Link to="/register/">Sign Up</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </header>
     </StyledNav>
   );
 }
